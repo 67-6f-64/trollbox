16fe05ad72212217bec081425d6f7cc7df4bafc5
Chris Rebel <chrebel@gmail.com>
2014-04-12 11:35:45 -0500

    Initial Commit

---
+++ trollbox/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  trollbox
+//
+//  Created by Chris Rebel on 4/12/14.
+//  Copyright (c) 2014 Chris Rebel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ trollbox/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  trollbox
+//
+//  Created by Chris Rebel on 4/12/14.
+//  Copyright (c) 2014 Chris Rebel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
